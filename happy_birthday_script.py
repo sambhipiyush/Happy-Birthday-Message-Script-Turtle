d0b605437864c9adf0b4e864059d2d3b9c20baa1
Piyush Sambhi <46954957+sambhipiyush@users.noreply.github.com>
2020-06-06 16:13:12 +0530

    Structured code for better readability

--- happy_birthday_script.py
+++ happy_birthday_script.py
@@ -1,34 +1,64 @@
-import turtle, random
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jun  6 16:11:50 2020
+
+@author: Piyush Sambhi
+ (                          (                              
+ )\ )                   )   )\ )                 )    )    
+(()/((  (      (     ( /(  (()/(    )    )    ( /( ( /((   
+ /(_))\ )\ )  ))\ (  )\())  /(_))( /(   (     )\()))\())\  
+(_))((_|()/( /((_))\((_)\  (_))  )(_))  )\  '((_)\((_)((_) 
+| _ \(_))(_)|_))(((_) |(_) / __|((_)_ _((_)) | |(_) |(_|_) 
+|  _/| | || | || (_-< ' \  \__ \/ _` | '  \()| '_ \ ' \| | 
+|_|  |_|\_, |\_,_/__/_||_| |___/\__,_|_|_|_| |_.__/_||_|_| 
+        |__/                                               
+
+@Contact-Details:
+    Email: piyush.sambhi07@icloud.com
+"""
+import turtle
+
+bgcolor = 'black'
+btm_lines_color='white'
+cake_color = 'white'
+candel_colors = ["red", "blue", "yellow", "green", "purple"]
+main_text_color = "orange"
+main_text = "Happy Birthday !!"
+final_pen_color = bgcolor
+
+font_type = "Comic Sans MS"
+font_size = 30
 
 # sets background
 bg = turtle.Screen()
-bg.bgcolor("black")
+bg.bgcolor(bgcolor)
 
 # Bottom Line 1
 turtle.penup()
 turtle.goto(-170,-180)
-turtle.color("white")
+turtle.color(btm_lines_color)
 turtle.pendown()
 turtle.forward(350)
 
 # Mid Line 2
 turtle.penup()
 turtle.goto(-160,-150)
-turtle.color("white")
+turtle.color(btm_lines_color)
 turtle.pendown()
 turtle.forward(300)
 
 # First Line 3
 turtle.penup()
 turtle.goto(-150,-120)
-turtle.color("white")
+turtle.color(btm_lines_color)
 turtle.pendown()
 turtle.forward(250)
 
 # Cake
 turtle.penup()
 turtle.goto(-100,-100)
-turtle.color("white")
+turtle.color(cake_color)
 turtle.begin_fill()
 turtle.pendown()
 turtle.forward(140)
@@ -43,32 +73,32 @@ turtle.end_fill()
 # Candles
 turtle.penup()
 turtle.goto(-90,0)
-turtle.color("red")
+turtle.color(candel_colors[0])
 turtle.left(180)
 turtle.pendown()
 turtle.forward(20)
 
 turtle.penup()
 turtle.goto(-60,0)
-turtle.color("blue")
+turtle.color(candel_colors[1])
 turtle.pendown()
 turtle.forward(20)
 
 turtle.penup()
 turtle.goto(-30,0)
-turtle.color("yellow")
+turtle.color(candel_colors[2])
 turtle.pendown()
 turtle.forward(20)
 
 turtle.penup()
 turtle.goto(0,0)
-turtle.color("green")
+turtle.color(candel_colors[3])
 turtle.pendown()
 turtle.forward(20)
 
 turtle.penup()
 turtle.goto(30,0)
-turtle.color("purple")
+turtle.color(candel_colors[4])
 turtle.pendown()
 turtle.forward(20)
 
@@ -88,8 +118,8 @@ for each_color in colors:
 # Happy Birthday message
 turtle.penup()
 turtle.goto(-150, 50)
-turtle.color("orange")
+turtle.color(main_text_color)
 turtle.pendown()
-turtle.write("Happy Birthday !!", move=False, align="left", font=("Comic Sans MS", 40, "normal"))
-turtle.color("black")
+turtle.write(main_text, move=False, align="left", font=(font_type, font_size, "normal"))
+turtle.color(final_pen_color)
 
